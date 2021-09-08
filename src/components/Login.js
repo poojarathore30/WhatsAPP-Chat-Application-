4cf039b557aac24d3035bd6248cf0763bc00054f
Pooja Rathore <poojarathore.nks@gmail.com>
2021-09-09 01:28:06 +0530

    Whatsapp clone

---
+++ src/components/Login.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+import { GoogleOutlined} from '@ant-design/icons'
+
+import firebase from "firebase/app"
+
+import { auth } from "../firebase"
+
+export default function Login() {
+  return (
+    <div id='login-page'>
+      <div id='login-card'>
+        <h2>Login to WhatsApp 💭</h2>
+
+        <div
+          className='login-button google'
+          onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
+        >
+          <GoogleOutlined /> Sign In with Google
+        </div>
+
+      </div>
+    </div>
+  )
+}
